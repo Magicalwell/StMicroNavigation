37b99ee097fa2c975d8adda4c2c1346d2a1e5419
jinglei <jinglei@jinglei>
2022-01-28 16:05:52 +0800

    初始化vue3+typescript的微前端项目

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <micro-app name='app1' url='http://localhost:8080/' baseroute='/my-page'></micro-app>
   </div>
 </template>
